e8f4182aec2a304416b7fab0cce744a6975bc17a
sunxiaoming1103@163.com <Xiaoming!@#123>
2018-03-24 20:01:14 +0800

    添加swagger 支持

---
+++ spring-study/src/main/java/demo/conf/Swagger.java
@@ -0,0 +1,30 @@
+package demo.conf;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class Swagger {
+
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo()).select()
+                .apis(RequestHandlerSelectors.basePackage("demo"))
+                .paths(PathSelectors.regex("(?!/error.*).*")).build();
+    }
+
+    @SuppressWarnings("deprecation")
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder().title("天王盖地虎api接口").description("说明RESTful APIs").contact("axiba@gmail.com").version("2.0")
+                .build();
+
+    }
+}
